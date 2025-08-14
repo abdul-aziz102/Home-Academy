2252bbcafc85f5afc2f51ff7e7e7c09dc3849b2f
Abdul Rehman <abdul@example.com>
2025-08-14 15:29:21 -0700

    he

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -14,7 +14,7 @@ const ChatbotWidget = () => {
         className="fixed bottom-4 right-4 z-50 flex items-center gap-2 bg-gradient-to-r from-indigo-500 to-purple-600 hover:opacity-90 text-white py-3 px-4 rounded-full shadow-lg cursor-pointer md:bottom-6 md:right-6"
       >
         <MessageCircle size={22} />
-        <span className="hidden sm:inline text-sm font-medium">
+        <span className=" text-sm font-medium">
           Ask Something From AI
         </span>
       </div>
